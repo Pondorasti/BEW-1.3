639b0eebbb5a1f0e6cd791679a81792763079860
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-01-21 09:20:18 -0800

    gif-search: styled page

--- gif-search/app.js
+++ gif-search/app.js
@@ -1,44 +1,47 @@
 // Require Libraries
-const express = require("express");
-const exphbs = require("express-handlebars");
+
+const express = require("express")
+const exphbs = require("express-handlebars")
 const Tenor = require("tenorjs").client({
   Key: "A8UV5083U09P",
   Filter: "high",
   Locale: "en_US",
-});
+})
 
 // App Setup
-const app = express();
+
+const app = express()
 
 // Middleware
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.use(express.static("public"))
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
 
 // Routes
 
 app.get("/", (req, res) => {
-  let term = "";
+  let term = ""
   if (req.query.term) {
-    term = req.query.term;
+    term = req.query.term
   }
 
   Tenor.Search.Query(term, "10")
     .then((response) => {
-      const gifs = response;
-      res.render("home", { gifs });
+      const gifs = response
+      res.render("home", { gifs })
     })
-    .catch(console.error);
-});
+    .catch(console.error)
+})
 
 app.get("/greetings/:name", (req, res) => {
-  const name = req.params.name;
+  const name = req.params.name
 
-  res.render("greetings", { name });
-});
+  res.render("greetings", { name })
+})
 
 // Start Server
 
 app.listen(3000, () => {
-  console.log("Hey! Gif search is up and running!");
-});
+  console.log("Hey! Gif search is up and running!")
+})
