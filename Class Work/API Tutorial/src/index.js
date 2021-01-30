70f3e6e8d4bc3a2c5d9971d65946f8baffa769a3
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-01-30 09:18:31 -0800

    Finished: API Tutorial

--- Class Work/API Tutorial/src/index.js
+++ Class Work/API Tutorial/src/index.js
@@ -1,31 +1,8 @@
 import "dotenv/config"
 import cors from "cors"
 import express from "express"
-import { v4 as uuidv4 } from "uuid"
-
-let users = {
-  1: {
-    id: "1",
-    username: "Robin Wieruch",
-  },
-  2: {
-    id: "2",
-    username: "Dave Davids",
-  },
-}
-
-let messages = {
-  1: {
-    id: "1",
-    text: "Hello World",
-    userId: "1",
-  },
-  2: {
-    id: "2",
-    text: "By World",
-    userId: "2",
-  },
-}
+import routes from "./routes"
+import models from "./models"
 
 const app = express()
 
@@ -33,49 +10,19 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use((req, res, next) => {
-  req.me = users[1]
+  req.context = {
+    me: models.users[1],
+    models,
+  }
   next()
 })
 
+app.use("/session", routes.session)
+app.use("/users", routes.user)
+app.use("/messages", routes.message)
+
 app.get("/", (req, res) => {
   res.send("Hello World!")
-})
-
-app.get("/session", (req, res) => {
-  res.send(req.me)
-})
-
-app.get("/users", (req, res) => {
-  return res.send(Object.values(users))
-})
-
-app.get("/users/:userId", (req, res) => {
-  return res.send(users[req.params.userId])
-})
-
-app.get("/messages", (req, res) => {
-  return res.send(Object.values(messages))
-})
-
-app.get("/messages/:messageId", (req, res) => {
-  return res.send(messages[req.params.messageId])
-})
-
-app.post("/messages", (req, res) => {
-  const id = uuidv4()
-  const message = { id, text: req.body.text, userId: req.me.id }
-
-  messages[id] = message
-
-  return res.send(message)
-})
-
-app.delete("/messages/:messageId", (req, res) => {
-  const { [req.params.messageId]: message, ...otherMessages } = messages
-
-  messages = otherMessages
-
-  return res.send(message)
 })
 
 app.listen(process.env.PORT, () => {
