0ead401edc12857488f484dfb323c0e356deeeba
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-02-02 21:03:47 -0800

    reddit: added created and updated timestamps for post

--- Class Work/reddit/models/post.js
+++ Class Work/reddit/models/post.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
-const PostSchema = new Schema({
-  title: { type: String, required: true },
-  url: { type: String, required: true },
-  summary: { type: String, required: true },
-})
+const PostSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    url: { type: String, required: true },
+    summary: { type: String, required: true },
+  },
+  { timestamps: { createdAt: "created_at" } }
+)
 
 export default mongoose.model("Posts", PostSchema)
