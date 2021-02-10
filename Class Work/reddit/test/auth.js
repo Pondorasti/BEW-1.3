9842edcf9682577e141526df31b04fc841f8720a
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-02-09 22:18:10 -0800

    reddit: Implemented initial authentication tests

---
+++ Class Work/reddit/test/auth.js
@@ -0,0 +1,39 @@
+import app from "../server.js"
+import chai from "chai"
+import chaiHttp from "chai-http"
+
+import User from "../models/user"
+// const User = require("../models/user")
+
+const should = chai.should()
+chai.use(chaiHttp)
+
+const agent = chai.request.agent(app)
+
+describe("User", function () {
+  it("should not be able to sign in if they are not registered", function (done) {
+    agent
+      .post("/login", { email: "no@no.com", password: "nono" })
+      .end(function (err, res) {
+        res.status.should.be.equal(401)
+        done()
+      })
+  })
+
+  it("should be able to signup", function (done) {
+    User.findOneAndRemove("test@test.com", function () {
+      agent
+        .post("/sign-up")
+        .send({ email: "test@test.com", password: "1234" })
+        .end(function (err, res) {
+          res.status.should.be.equal(200)
+          agent.should.have.cookie("nToken")
+          done()
+        })
+    })
+  })
+
+  after(function () {
+    agent.close()
+  })
+})
