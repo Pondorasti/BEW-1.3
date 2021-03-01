7b649cbdb4e6bd11bbfaebffa213d1dfa99ab40a
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-03-01 11:43:44 -0800

    custom-api: Add body-parser

--- Class Work/custom-api/src/index.ts
+++ Class Work/custom-api/src/index.ts
@@ -1,14 +1,23 @@
 import express from "express"
+import bodyParser from "body-parser"
 import { rootHandler, helloHandler } from "./handlers"
-
+import router from "./routes"
 import "./config/databaseSetup"
 
+// Setup
 const app = express()
 const port = process.env.PORT || 8000 // Database Setup
 
+// Middlewares
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Routes
 app.get("/", rootHandler)
 app.get("/hello/:name", helloHandler)
+app.use(router)
 
+// Start Server
 app.listen(port, () => {
   console.log(`server is listening on ${port}`)
 })
