353fbf554465d024b093a80293d738f854ad2c81
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-03-03 22:51:36 -0800

    custom-api: Add .eslintignore

--- Class Work/custom-api/src/handlers.ts
+++ Class Work/custom-api/src/handlers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
 
-const rootHandler = (_req: Request, res: Response) => res.send("API is working")
+const rootHandler = (_req: Request, res: Response): Response => res.send("API is working")
 
 export default rootHandler
